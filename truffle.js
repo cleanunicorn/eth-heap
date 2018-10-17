9be15e62d2a13b3cd0c1b2c38527dd4a69434d19
Daniel Luca <lucadanielcostin@gmail.com>
2018-10-17 20:12:32 +0300

    Add fuzzy test

--- truffle.js
+++ truffle.js
@@ -1,19 +1,38 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs')
+
+// First read in the secrets.json to get our mnemonic
+let secrets
+let mnemonic
+let infuraKey
+if (fs.existsSync('secrets.json')) {
+    secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
+    mnemonic = secrets.mnemonic
+    infuraKey = secrets.infuraKey
+} else {
+    console.log('No secrets.json found. If you are trying to publish EPM ' +
+        'this will fail. Otherwise, you can ignore this message!')
+    mnemonic = ''
+}
 
 module.exports = {
-
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: 'localhost',
+            port: 7545,
+            network_id: '42', // eslint-disable-line camelcase
+            gasPrice: 0x01,
+        },
+        coverage: {
+            host: "localhost",
+            network_id: "*",
+            port: 8555,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01
+        },
+        ropsten: {
+            provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + infuraKey),
+            network_id: '3'
+        },
+    },
 };
